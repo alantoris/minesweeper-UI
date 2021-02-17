f052f18345d163a0ee80b8f015db9a75cab0fa9c
Alan <alan.toris@gmail.com>
2021-02-17 16:07:30 -0300

    set 50 as board max width and max height

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export const BOARD_WIDTH_DEFAULT = 20
 export const BOARD_HEIGHT_DEFAULT = 20
 
 export const BOARD_MIN_WIDTH = 10
-export const BOARD_MAX_WIDTH = 100
+export const BOARD_MAX_WIDTH = 50
 export const BOARD_MIN_HEIGHT = 10
-export const BOARD_MAX_HEIGHT = 100
+export const BOARD_MAX_HEIGHT = 50
